9c8686f3a51a259cf380cbf325b4c718dbec0ef9
AhmedMohammed100 <ahmedtal650@gmail.com>
2024-10-04 03:09:08 +0000

    go

--- mine.js
+++ mine.js
@@ -1,41 +1,5 @@
-let miningActive = false;
-let miningInterval;
-let balance = 0;
-let minedCoins = 0;
+// script.js
 
-const balanceDisplay = document.getElementById('balance');
-const statusDisplay = document.getElementById('status');
-const mineButton = document.getElementById('mine-btn');
-const collectButton = document.getElementById('collect-btn');
-
-// Function to start mining
-mineButton.addEventListener('click', function () {
-  if (!miningActive) {
-    miningActive = true;
-    mineButton.innerHTML = 'Stop Mining';
-    statusDisplay.innerHTML = 'Mining started...';
-
-    // Start mining process (earn 1 coin every second)
-    miningInterval = setInterval(() => {
-      minedCoins++;
-      statusDisplay.innerHTML = `You have mined ${minedCoins} coins.`;
-    }, 1000);
-  } else {
-    miningActive = false;
-    mineButton.innerHTML = 'Start Mining';
-    statusDisplay.innerHTML = 'Mining stopped.';
-    clearInterval(miningInterval);
-  }
-});
-
-// Function to collect mined coins and update balance
-collectButton.addEventListener('click', function () {
-  if (minedCoins > 0) {
-    balance += minedCoins;
-    balanceDisplay.innerHTML = balance;
-    statusDisplay.innerHTML = `You collected ${minedCoins} coins!`;
-    minedCoins = 0;
-  } else {
-    statusDisplay.innerHTML = 'No coins to collect!';
-  }
-});
+function toggleLike(button) {
+  button.classList.toggle('liked');
+}
