f778dc03cb322a410b43b6b0f2ae34ce6cd797af
AhmedMohammed100 <ahmedtal650@gmail.com>
2024-10-05 02:26:18 +0000

    wallet

--- mywallet/app/layout.tsx
+++ mywallet/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <title>Big Farm Wallet Connect</title>
       </head>
       <body>
-        <TonConnectUIProvider manifestUrl="https://mega.nz/file/5L0HEbLB#6mqiNA-rrM_HMtAz6JJW6FlNGGNYgavGBYLuJnAp9gI">
+        <TonConnectUIProvider manifestUrl="https://mega.nz/file/9eFSESyJ#T7DxaCv4tz-NoGc09YWo3fROjumprBvAahaujycprKc">
         {children}
         </TonConnectUIProvider>
       </body>
